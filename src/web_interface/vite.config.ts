cd0542bbec913970dcbecd039de1f7d507770733
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-06-22 09:52:29 +0200

    First commit web_interface

---
+++ src/web_interface/vite.config.ts
@@ -0,0 +1,18 @@
+// ts-ignore-errors
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: "../server/templates/",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        red_team: resolve(__dirname, "red_team.html"),
+      },
+    },
+  },
+});
