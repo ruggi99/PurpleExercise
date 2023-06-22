9901c8d5154d135ba09e914d53fe2ad44767c187
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-06-22 12:34:21 +0200

    Change builder for frontend

---
+++ src/web_interface/build.js
@@ -0,0 +1,10 @@
+import fs from "node:fs/promises";
+import util from "node:util";
+import {execSync} from "node:child_process";
+
+const response = execSync("npx vite build");
+console.log(response.toString())
+await fs.rename("dist/index.html", "../server/templates/index.html");
+await fs.rename("dist/red_team.html", "../server/templates/red_team.html");
+await fs.rm("../server/static/", { recursive: true, force: true });
+await fs.rename("dist/static/", "../server/static/");
--- src/web_interface/src/build.sh
+++
@@ -1,7 +0,0 @@
-out_folder = "assets";
-
-npm run build
-
-mv out_folder/*.js ../../server/static/js
-mv out_folder/*.css ../../server/static/css
-rm -r out_folder
