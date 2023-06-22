d964453e0fb9cf87ca5d30d6fe14731e5ae646c5
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-06-22 10:55:39 +0200

    Add index page

--- src/web_interface/src/index.ts
+++ src/web_interface/src/index.ts
@@ -1,7 +1,7 @@
 import './app.css'
-import App from './App.svelte'
+import Index from './Index.svelte'
 
-const app = new App({
+const app = new Index({
   target: document.getElementById('app'),
 })
 
