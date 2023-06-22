d75f020aaf539d92630cdfc3cb456e6fd0e644be
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-06-22 10:56:33 +0200

    Create types.ts

---
+++ src/web_interface/src/types.ts
@@ -0,0 +1,12 @@
+export type DataType = {
+    points: number;
+    initial_points: number;
+    max_seconds_available: number;
+    start_time: number;
+    game_ended: boolean;
+}
+
+export type InitialDataType = {
+    points: number;
+    max_points: number;
+}
