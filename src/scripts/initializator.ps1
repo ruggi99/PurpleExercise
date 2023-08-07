cac40b72ad9c7e4391bebc43862ce8d546bdede6
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-08-07 09:44:07 +0200

    Remove useless variables in initializator.ps1

--- src/scripts/initializator.ps1
+++ src/scripts/initializator.ps1
@@ -3,27 +3,11 @@ Import-Module -force "$($UTILS_PATH)config.ps1"
 Import-Module -force "$($UTILS_PATH)Add-ADUser.ps1"
 
 function Write-Good { param($String) Write-Host $Global:PlusLine $String -ForegroundColor 'Green' }
-function Write-Bad { param($String) Write-Host $Global:ErrorLine $String -ForegroundColor 'red' }
+function Write-Bad  { param($String) Write-Host $Global:ErrorLine $String -ForegroundColor 'red' }
 function Write-Info { param($String) Write-Host $Global:InfoLine $String -ForegroundColor 'gray' }
-
-# AD INITIALIZATION
-# Define configuration file path
-$configPath = "AD_network.json"
-
-# Check configuration file path
-if (-not (Test-Path -Path $configPath)) {
-    # Configuration file not found
-    throw "Configuration file not found. Check file path."
-}
-
-# Load configuration file
-$config = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 
 # Define the domain name
 $Domain = $config.domain.name
-
-# Define users limit
-$UsersLimit = $config.domain.usersLimit
 
 # Create credential object for the local admin
 $admin = New-Object System.Management.Automation.PSCredential -ArgumentList $($config.domain.admin),(ConvertTo-SecureString -String $config.domain.password -AsPlainText -Force)
